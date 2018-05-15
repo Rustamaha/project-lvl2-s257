a0cfef26272ca4fcae7f65e123b25052750e523f
Rustam <ruslyandiya@mail.com>
2018-05-15 18:09:47 +0300

    'gendiff_import'

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -9,5 +9,5 @@ program
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .action((firstConfig, secondConfig) => genDiff(firstConfig, secondConfig))
+  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)))
   .parse(process.argv);
