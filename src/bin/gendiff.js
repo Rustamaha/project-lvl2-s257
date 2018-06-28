8ea6b60232473bf7838e042ccecf48fb5a2bd393
Rustam <ruslyandiya@mail.ru>
2018-06-28 17:35:34 +0300

    'fix_commander'

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,11 +8,11 @@ import packageJson from '../../package.json';
 
 program
   .version(packageJson.version)
-  .arguments('<firstConfig> <secondConfig>')
+  .arguments('[option] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format: plain, json, standart')
   .action((firstConfig, secondConfig) => {
-    const result = genDiff(firstConfig, secondConfig, program.format);
+    const result = genDiff(firstConfig, secondConfig, option);
     console.log(result);
   });
 
